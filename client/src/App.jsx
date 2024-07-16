f0e3b556cbdc6e88caef076b81de89d68be42218
Priyanshu Kumar Mandal <mandalpriyanshu22@gmail.com>
2024-07-16 21:09:42 +0545

    Header section created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,29 @@
-import { BrowseRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+
+
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
 
 function App() {
 
   return (
     <>
-     <BrowseRouter>
+     <BrowserRouter>
+     <Header/>
      <Routes>
-      <Route path='/' element={<Home/>}/>
+      <Route path='/' element={<Home />}/>
       <Route path='/about' element={<About/>}/>
       <Route path='/sign-in' element={<SignIn/>}/>
       <Route path='/sign-up' element={<SignUp/>}/>
-      <Route path='/search' element={<Search/>}/>
       <Route path='/dashboard' element={<Dashboard />} />
       <Route path='/projects' element={<Projects/>}/>
-
      </Routes>
-     </BrowseRouter>
+     </BrowserRouter>
     </>
   )
 }
