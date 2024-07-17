df58e1376f86ac7298b2c5354ceab06c6745ebdd
Priyanshu Kumar Mandal <mandalpriyanshu22@gmail.com>
2024-07-17 17:16:40 +0545

    sign up ui fixed

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Label, Button, TextInput} from 'flowbite-react'
+import { Label, Button, TextInput } from "flowbite-react";
 
 function SignUp() {
   return (
     <div className="min-h-screen mt-20">
-      <div className="flex p-3 max-w-auto mx-auto flex-col md:flex-row md:items-center gap-5">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
         {/* left */}
         <div className="flex-1">
           <Link to="/" className="font-bold dark:text-white text-4xl">
@@ -39,20 +39,19 @@ function SignUp() {
               <Label value="Your password" />
               <TextInput type="password" placeholder="Password" id="password" />
             </div>
-            <Button gradientDuoTone="purpleToPink" type="submit">Sign Up</Button>
+            <Button gradientDuoTone="purpleToPink" type="submit" />
           </form>
-          <div className="flex gap-2 text-sm mt-5
-          ">
-            <span>
-              Have an account?
-            </span>
-            <Link to='/sign-in' className="text-blue-500">
-            Sign In
+          <div
+            className="flex gap-2 text-sm mt-5
+          "
+          >
+            <span>Have an account?</span>
+            <Link to="/sign-in" className="text-blue-500">
+              Sign In
             </Link>
           </div>
         </div>
       </div>
-      
     </div>
   );
 }
