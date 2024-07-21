d576dd12cc1b5bdaa2bca52f44379fe2f2852be0
Priyanshu Kumar Mandal <mandalpriyanshu22@gmail.com>
2024-07-21 21:50:24 +0545

    add delete user api route

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from 'bcryptjs';
 import { errorHandler} from '../utils/error.js';
 import User from '../models/user.model.js';
+import e from 'express';
 
 
 export const test = (req, res) => {
@@ -53,5 +54,17 @@ export const updateUser = async (req, res, next) => {
       } catch (error) {
         next(error);
       }
+    }
+  };
+
+  export const deleteUser = async (req, res, next) => {
+    if (req.user.id !== req.params.userId) {
+      return next(errorHandler(403, 'You are not allowed to delete this user'));
+    }
+    try {
+      await User.findByIdAndDelete(req.params.userId);
+      res.status(200).json('User has been deleted');
+    } catch (error) {
+      next(error);
     }
   };
